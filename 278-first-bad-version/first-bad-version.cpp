7e01083e0514e8c9822880b48cf7616ba46ef9a7
Danish-Awasthi <awasthidanish2003@gmail.com>
2024-09-01 17:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (59.60%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -5,19 +5,25 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int start=0,end=n,m;
-        while(start<=end){
+        while(start<end){
             m=start+(end-start)/2;
-            int res= isBadVersion(m);
-            if(res==1 && (m==0 || isBadVersion(m-1)!=1)){
-                return m;
-            }
-            else if(res==0){
-                start=m+1;
-            }
-            else{
-                end=m-1;
+            // int res= isBadVersion(m);
+            // if(res==1 && (m==0 || isBadVersion(m-1)!=1)){
+            //     return m;
+            // }
+            // else if(res==0){
+            //     start=m+1;
+            // }
+            // else{
+            //     end=m-1;
+            // }
+            if (isBadVersion(m)) {
+                end = m;
+            } else {
+                start = m + 1;
             }
         }
-        return m;
-    }
+        return start;
+        }
+        // return m;
 };
