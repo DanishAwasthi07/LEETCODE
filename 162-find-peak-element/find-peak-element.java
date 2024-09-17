0ecede7320bbc687624aa88d6a295a6e42ece86f
Danish-Awasthi <awasthidanish2003@gmail.com>
2024-09-17 10:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.79%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -11,8 +11,8 @@ class Solution {
             mid= s +(e-s)/2;
             if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
             else if(nums[mid]>nums[mid-1]) s=mid+1;
-            else if(nums[mid]>nums[mid+1]) e=mid-1;
-            else e=mid-1;
+            else  e=mid-1;
+            // else e=mid-1;
         }
         return -1;
     }
