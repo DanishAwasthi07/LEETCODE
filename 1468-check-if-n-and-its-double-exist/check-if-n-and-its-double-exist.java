a59cceabb800e55f5d201aa6e8332b8204cb391c
Danish Awasthi <awasthidanish2003@gmail.com>
2024-12-01 23:44:11 +0530

    Time: 2 ms (93.52%) | Memory: 43 MB (33.23%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer>set= new HashSet<>();
+        for(int ele:arr){
+           if(set.contains(ele*2)||(ele%2==0 && set.contains(ele/2)))return true;
+           set.add(ele);
+        }
+        return false;
+    }
+}
